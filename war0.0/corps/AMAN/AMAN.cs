0a5d92cf5b38b581ddcb9afbc8841409823a6f40
benjamin <2000beni770@gmail.com>
2025-05-22 14:57:50 +0300

    add addRandomTerrorist to AMAN

--- war0.0/corps/AMAN/AMAN.cs
+++ war0.0/corps/AMAN/AMAN.cs
@@ -1,3 +1,5 @@
+using Bogus;
+
 namespace War;
 
 public class AMAN : Corps
@@ -12,6 +14,19 @@ public class AMAN : Corps
     public void addTerorist(float weight = 1, string name = "anonymous", int age = 0, bool isAlive = true, int liveRange = 1)
     {
         Soldier terrorist = new Soldier(weight, name,  age, isAlive, liveRange);
+        knownTerorists.Add(terrorist);
+    }
+    
+    // creating a func that will add a terrorist with random values
+    public void addRandomTerorist()
+    {
+        Random rnd = new Random();
+        int weight = rnd.Next(20, 90);
+        int age = rnd.Next(15, 80);
+        
+        var faker = new Faker();
+        string randomName = faker.Name.FullName();
+        Soldier terrorist = new Soldier(weight, randomName,  age, true, 5);
         knownTerorists.Add(terrorist);
     }
     
