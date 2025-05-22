a4faf4d3b4f42a2903e3e2edb8752aa2dc311df2
benjamin <2000beni770@gmail.com>
2025-05-22 14:31:31 +0300

    add constractor to AMAN

--- war0.0/corps/AMAN/AMAN.cs
+++ war0.0/corps/AMAN/AMAN.cs
@@ -3,11 +3,18 @@ namespace War;
 public class AMAN : Corps
 {
     private List<Soldier> knownTerorists = new List<Soldier>();
+
+    public AMAN()
+    {
+        Console.WriteLine("AMAN was creatd");
+    }
+    
     public void addTerorist(float weight = 1, string name = "anonymous", int age = 0, bool isAlive = true, int liveRange = 1)
     {
         Soldier terrorist = new Soldier(weight, name,  age, isAlive, liveRange);
         knownTerorists.Add(terrorist);
     }
+    
 
     public void getAllTerorists()
     {
