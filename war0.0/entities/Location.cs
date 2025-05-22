4512a1c4ae7440ae620d50c9400505f80be44e97
Tzvi Zait <z0548430441@gmail.com>
2025-05-22 13:03:27 +0300

    add location

---
+++ war0.0/entities/Location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War
+{
+    internal class Location 
+    {
+        public double Latitude;
+        public double Longitude;
+        public double Altitude;
+
+        public Location(double latitude = 0, double longitude = 0, double altitude = 0)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Altitude = altitude;
+        }
+
+        public double[] getLocation()
+        {
+            double[] res = new double[3];
+            res[0] = Latitude;
+            res[1] = Longitude;
+            res[2] = Altitude;
+            return res;
+        }
+
+        public void setLocation(double latitude, double longitude, double altitude)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Altitude = altitude;
+
+        }
+    }
+}
