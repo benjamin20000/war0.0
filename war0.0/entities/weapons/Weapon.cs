f37a5c03c140ddf44a5f1f350d373334212661ef
benjamin <2000beni770@gmail.com>
2025-05-27 13:01:35 +0300

    edit weapon func

--- war0.0/entities/weapons/Weapon.cs
+++ war0.0/entities/weapons/Weapon.cs
@@ -5,10 +5,11 @@ public class Weapon : PhysicalEntity
     int TotalAmmo;
     int MaxAmmo;
     string AmmoType;
-    public Weapon(int maxAmmo, float weight = 1) : base(weight)
+    public Weapon(int maxAmmo=10, float weight = 1, string ammoType = "nurmal bulet") : base(weight)
     {
         this.MaxAmmo = maxAmmo;
         this.TotalAmmo = maxAmmo;
+        this.AmmoType = ammoType;
         Console.WriteLine($"weapon created");
     }
     public void shoot()
@@ -21,6 +22,15 @@ public class Weapon : PhysicalEntity
         else
         {
             Console.WriteLine($"no enough bullets..");
+        }
+    }
+
+    public void addAmmo(int ammo_amount)
+    {
+        this.TotalAmmo += ammo_amount;
+        if (TotalAmmo > MaxAmmo)
+        {
+            TotalAmmo = MaxAmmo;
         }
     }
 }
